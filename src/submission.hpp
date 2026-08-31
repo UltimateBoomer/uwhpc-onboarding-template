b2d7f146284caa582dbc5deb1466b72886e8b549
Steven Cao <s3cao@uwaterloo.ca>
2026-08-31 18:45:37 -0400

    Implement apply stencil not working yet

--- src/submission.hpp
+++ src/submission.hpp
@@ -16,17 +16,39 @@ private:
 
 public:
   // Initializes a zero-filled grid with specified size
-  Grid(std::size_t rows, std::size_t cols) : rows_(rows_), cols_(cols), data(rows * cols) {}
+  Grid(std::size_t rows, std::size_t cols)
+      : rows_(rows), cols_(cols), data(rows * cols) {}
 
-  double& operator()(std::size_t i, std::size_t j) {
-      return data[i * rows_ + j];
+  double &operator()(std::size_t i, std::size_t j) {
+    return data[i * rows_ + j];
   }
 
   double operator()(std::size_t i, std::size_t j) const {
-      return (*this)(i, j);
+    return (*const_cast<Grid*>(this))(i, j);
   }
+
+  std::size_t get_rows() const { return rows_; }
+  std::size_t get_cols() const { return cols_; }
 };
 
 // Apply the five-point stencil over all interior points, copying the boundary
 // values unchanged from old_grid to new_grid. Implement your solution here.
-void apply_stencil(const Grid& old_grid, Grid& new_grid);
+void apply_stencil(const Grid &old_grid, Grid &new_grid) {
+  for (std::size_t i = 0; i < new_grid.get_rows(); ++i) {
+    for (std::size_t j = 0; j < new_grid.get_cols(); ++j) {
+      if (i == 0 || i == new_grid.get_rows() - 1 || j == 0 || j == new_grid.get_cols() - 1) {
+        // Boundary cell, keep old value
+        new_grid(i, j) = old_grid(i, j);
+      } else {
+        // Internal cell, apply kernel
+        double s1 = old_grid(i - 1, j);
+        double s2 = old_grid(i, j - 1);
+        double s3 = old_grid(i, j);
+        double s4 = old_grid(i, j + 1);
+        double s5 = old_grid(i + 1, j);
+
+        new_grid(i, j) = 0.125 * (s1 + s2 + s4 + s5) + 0.5 * s3;
+      }
+    }
+  }
+}
